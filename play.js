c8bee87b8cd8452cc9707d7a0fa7b70e738a8de7
Andrew Roth <a.roth.80@hotmail.com>
2024-11-19 18:52:59 -0800

    client.js created and connection module moved into it from play.js

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require('net');
-
-// establish a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-// event handler for messages from server
-  conn.on('data', (data) => {
-    console.log('server says :', data);
-  });
-// encodes messages from server to utf8
-  conn.setEncoding('utf8');
-  
-  return conn;
-};
-
+const connect = require("./client");
 
 
 console.log('Connecting...');
