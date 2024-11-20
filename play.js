aa8c480a4b0daf82f4c97b958265bcb1abcef7d9
Andrew Roth <a.roth.80@hotmail.com>
2024-11-19 18:32:58 -0800

    snake multiplayer connection and message handler added

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+// establish a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+// event handler for messages from server
+  conn.on('data', (data) => {
+    console.log('server says :', data);
+  });
+// encodes messages from server to utf8
+  conn.setEncoding('utf8');
+  
+  return conn;
+};
+
+
+
+console.log('Connecting...');
+connect();
