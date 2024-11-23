a70ef1f1948484530b7b9f55a6485a8627bccf46
Andrew Roth <a.roth.80@hotmail.com>
2024-11-22 23:28:26 -0800

    constants.js added and clients.js modified

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require('net');
+const {IP, PORT } = require('./constants');
 
 // establish a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // event handler for messages from server
   conn.on('data', (data) => {
